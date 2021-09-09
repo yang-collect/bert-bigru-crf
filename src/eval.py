776e520b84be48489f3c47ef252e7b96c1a2e58c
Carlos yang <46920280+yang-collect@users.noreply.github.com>
2021-09-10 02:18:21 +0800

    Update eval.py

--- src/eval.py
+++ src/eval.py
@@ -8,7 +8,7 @@ import config
 
 def evaluate(path, tokenizer, model):
     val_data = DataLoad(tokenizer, path)
-    avg_acc=[]
+    avg_recall=[]
     with torch.no_grad():
         device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
         for batch, data in enumerate(val_data):
@@ -18,17 +18,17 @@ def evaluate(path, tokenizer, model):
             predictions = [item + [12] * (config.max_length - len(item)) if len(item) < config.max_length else item for
                            item in predictions]
             predictions=torch.tensor(predictions) # 转化为tensor
-            acc=(predictions==data['labels']).sum()/predictions.numel() # 计算acc值 ，predictions.numel() shape的累积
-            avg_acc.append(acc.numpy())
-    return np.mean(avg_acc)
+            recall=((predictions==data['labels'])&(data['labels']!=12)).sum()/(data['labels']!=12).sum() # 计算acc值 ，predictions.numel() shape的累积
+            avg_recall.append(recall.numpy())
+    return np.mean(avg_recall)
 
 
 if __name__ == '__main__':
     tokenizer = BertTokenizer.from_pretrained(config.save_path)
     model = Bert_BiGru_Crf.from_pretrained(config.save_path)
+    # 有效部分是指非O的部分
+    print('train data 的有效部分召回为:', evaluate(config.train_path, tokenizer, model))
 
-    print('train data  score:', evaluate(config.train_path, tokenizer, model))
+    print('test data 的有效部分召回为:', evaluate(config.test_path, tokenizer, model))
 
-    print('test data  score:', evaluate(config.test_path, tokenizer, model))
-
-    print('dev data  score:', evaluate(config.dev_path, tokenizer, model))
+    print('dev data 的有效部分召回为:', evaluate(config.dev_path, tokenizer, model))
